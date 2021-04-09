954ca23bc0646c782a46fce04a2c3cb6ec27585c
Deepak Dinakaran <deepakdinakaran22@gmail.com>
2021-04-10 01:09:10 +0530

    Added UnitOfWork

---
+++ Aris.Data/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using Aris.Data.Entities;
+using System;
+
+namespace Aris.Data
+{
+    public class UnitOfWork : IDisposable
+    {
+        private ArisContext context = new ArisContext();
+        private GenericRepository<Users> userRepository;
+
+        public GenericRepository<Users> UserRepository
+        {
+            get
+            {
+                if (this.userRepository == null)
+                    this.userRepository = new GenericRepository<Users>(context);
+                return this.userRepository;
+
+            }
+
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
